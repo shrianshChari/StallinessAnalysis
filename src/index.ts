66d641f7b91f93d866131ddf4fbfbe6ef00e0a66
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2022-03-04 10:59:12 -0500

    Created checks for file existence

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import * as fs from 'fs';
 import { importTeam } from './sets'
 
-let file_path = 'data/' + process.argv[2]
+if (process.argv.length < 3) {
+	console.log('You need to specify a file to read from!')
+} else {
+	let file_path = 'data/' + process.argv[2]
 
-console.log(`Reading data from: ${file_path}`);
+	console.log(`Reading data from: ${file_path}`);
 
-let team_file = fs.readFile(file_path, 'utf8', (err, data) => {
-	if (err) {
-		console.log(`An error occurred while reading ${process.argv[2]}`)
-		console.log(err)
-		return;
-	}
-	let pokemon_sets = importTeam(data)
+	fs.readFile(file_path, 'utf8', (err, data) => {
+		if (err) {
+			console.log(`An error occurred while reading ${process.argv[2]}`)
+			console.log(err)
+			return;
+		}
+		let pokemon_sets = importTeam(data)
 
-	// Removing uppercase, spaces, and dashes
-	pokemon_sets = JSON.parse(JSON.stringify(pokemon_sets).toLowerCase().replace(' ', '').replace('-', ''))
-	console.log(`${JSON.stringify(pokemon_sets)}`) 
-})
+		// Removing uppercase, spaces, and dashes
+		pokemon_sets = JSON.parse(JSON.stringify(pokemon_sets).toLowerCase().replace(' ', '').replace('-', ''))
+		console.log(`${JSON.stringify(pokemon_sets)}`) 
+	})
+}
